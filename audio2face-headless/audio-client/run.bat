d7bc62e6ef104ac67cdfef925789e99998e51601
vincentz <vincentz@nvidia.com>
2023-03-19 00:18:49 -0700

    audio2face-headless - update

--- audio2face-headless/audio-client/run.bat
+++ audio2face-headless/audio-client/run.bat
@@ -1,2 +1,2 @@
-@REM pip install protobuf==3.17.3 gprcio soundfile
-python test_client.py xmas.mp3 /World/audio2face/PlayerStreaming
+@REM pip install protobuf==3.17.3 grpcio soundfile
+python test_client.py %1 /World/audio2face/PlayerStreaming
