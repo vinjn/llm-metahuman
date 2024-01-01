5a92fb0855b478d79a1afcd94c373f468c3f5c3a
vincentz <vincentz@nvidia.com>
2023-12-31 22:42:26 -0800

    more

--- audio2face-headless/audio-client/portal.py
+++ audio2face-headless/audio-client/portal.py
@@ -1,15 +1,15 @@
 import gradio as gr
 
 
-# Code for Task 1
 def task1(input_text):
-    # Task 1 logic
     return "Task 1 Result: " + input_text
 
 
-# Code for Task 2
 def task2(input_image):
-    # Task 2 logic
+    return "Task 2 Result"
+
+
+def task3(input_image):
     return "Task 2 Result"
 
 
@@ -22,7 +22,32 @@ iface2 = gr.Interface(
     fn=task2, inputs="image", outputs="text", title="Multi-Page Interface"
 )
 
-demo = gr.TabbedInterface([iface1, iface2], ["Text-to-text", "image-to-text"])
+tts_examples = [
+    "I love learning machine learning",
+    "How do you do?",
+]
+
+
+tts_demo = gr.load(
+    "huggingface/facebook/fastspeech2-en-ljspeech",
+    title=None,
+    examples=tts_examples,
+    description="Give me something to say!",
+    cache_examples=False,
+)
+
+stt_demo = gr.load(
+    "huggingface/facebook/wav2vec2-base-960h",
+    title=None,
+    inputs="mic",
+    description="Let me try to guess what you're saying!",
+)
+
+
+demo = gr.TabbedInterface(
+    [iface1, iface2, tts_demo, stt_demo],
+    ["Text-to-text", "image-to-text", "Text-to-speech", "Speech-to-text"],
+)
 
 # Run the interface
 demo.launch(share=True)
